4617c87f195670fc89affa8256ad202ec91cbcc8
Noa Caspi <noacaspi98@gmail.com>
2025-08-13 07:47:32 +0300

    fix generator

--- generator.py
+++ generator.py
@@ -4,12 +4,12 @@ import torch
 def generate_w(sequence_length: int,
                      device: torch.device):
     # instead of the dataset, we generate w (the first measurement without the last time step, reversed)
-    w = torch.normal(mean=0, std=1, size=sequence_length-1, device=device)
+    w = torch.normal(mean=0, std=1, size=(sequence_length-1,), device=device)
     return w
 
 
 def generate_teacher_alpha(device):
-    return torch.normal(mean=0.5, std=0.1, size=1, device=device)
+    return torch.normal(mean=0.5, std=0.1, size=(1,), device=device)
 
 
 def generate_students(student_dim: int, bs: int, device: torch.device):
