5c23d3de983239c3abd85f90c5846508ae56640c
noacaspi <nocaspi@mail.tau.ac.il>
2025-11-17 11:32:12 +0200

    add debug

--- keras_model/run_expirement.py
+++ keras_model/run_expirement.py
@@ -24,15 +24,19 @@ sd_B_C = 0.001
 diff = 0.05 / np.exp(5 * np.log10(1 / sd_A))
 
 
-def run_experiment(train_inputs, train_outputs, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs):
+def run_experiment(train_inputs, train_outputs, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs, debug=False):
     train_losses, ext_losses = [], []
     for train_inputs, train_outputs, seed in zip(train_inputs, train_outputs, seeds):
+        if debug:
+            print(f"Running experiment with seed {seed} on {train_inputs} and train outputs {train_outputs}")
         train_loss, ext_loss = train(train_inputs, train_outputs, ext_inputs, ext_outputs, student_state_dim, seed, sd_A, 
                                     sd_B_C, base_lr, epochs, eps, diff, warm_init=warm_init, adaptive=adaptive, 
                                     log_period=log_period, print_period=print_period, epochs_after_opt=epochs_after_opt, 
                                     exper_type=exper_type, fix_B_C=False)
         train_losses.append(train_loss)
         ext_losses.append(ext_loss)
+        if debug:
+            print(f"Train loss: {train_loss}, Ext. loss: {ext_loss}")
     print("-------------------------------------------------------------------------")
     print("-------------------------------------------------------------------------")
     print(f'Average train loss: {np.mean(train_losses)}')
@@ -69,7 +73,7 @@ def beyond_theory_one(alpha_teacher, adaptive, student_state_dim, length, ext_le
         baseline_input[:, 0:2, :] = 1
         train_inputs_baseline = [generate_inputs(1, sd_baseline, sd_special, seed=seed, baseline_input=baseline_input) for seed in seeds]
         train_outputs_baseline = [teacher(train_inputs) for train_inputs in train_inputs_baseline]
-        train_losses_baseline, ext_losses_baseline = run_experiment(train_inputs_baseline, train_outputs_baseline, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs)
+        train_losses_baseline, ext_losses_baseline = run_experiment(train_inputs_baseline, train_outputs_baseline, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs, debug=debug)
 
         print("-------------------------------------------------------------------------")
         print("-------------------------------------------------------------------------")
@@ -87,7 +91,7 @@ def beyond_theory_one(alpha_teacher, adaptive, student_state_dim, length, ext_le
         train_inputs_poison = [generate_inputs(1, sd_baseline, sd_special, seed=seed, baseline_input=baseline_input, 
                                         special_input=special_input) for seed in seeds]
         train_outputs_poison = [teacher(train_inputs) for train_inputs in train_inputs_poison]
-        train_losses_poison, ext_losses_poison = run_experiment(train_inputs_poison, train_outputs_poison, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs)
+        train_losses_poison, ext_losses_poison = run_experiment(train_inputs_poison, train_outputs_poison, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs, debug=debug)
 
         print("-------------------------------------------------------------------------")
         print("-------------------------------------------------------------------------")
@@ -105,7 +109,7 @@ def beyond_theory_one(alpha_teacher, adaptive, student_state_dim, length, ext_le
         train_inputs[2, :, :] = [[1], [0], [0], [0], [0]]
         train_inputs[3, :, :] = [[1], [1], [1], [1], [1]]
         train_outputs = [teacher(ipt) for ipt in train_inputs]
-        train_losses, ext_losses = run_experiment(train_inputs, train_outputs, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs)
+        train_losses, ext_losses = run_experiment(train_inputs, train_outputs, ext_inputs, ext_outputs, adaptive, student_state_dim, seeds, base_lr, eps, epochs, debug=debug)
         print("-------------------------------------------------------------------------")
         print("-------------------------------------------------------------------------")
         print("Summary:")
@@ -132,6 +136,7 @@ def parse_args():
     parser.add_argument('--with_baseline_in_poison', type=bool, default=True)
     parser.add_argument('--fixed_inputs', type=bool, default=False)
     parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--debug', type=bool, default=False)
     return parser.parse_args()
 
 
@@ -149,9 +154,10 @@ if __name__ == "__main__":
     num_baseline = args.num_baseline
     num_special = args.num_special
     with_baseline_in_poison = args.with_baseline_in_poison
+    debug = args.debug
     physical_devices = tf.config.list_physical_devices('GPU')
     if len(physical_devices) > args.gpu:
         tf.config.set_visible_devices(physical_devices[args.gpu:args.gpu+1], 'GPU')
 
 
-    beyond_theory_one(alpha_teacher, adaptive, student_state_dim, length, ext_length, eps, fixed_inputs, base_lr, epochs, num_baseline, num_special, with_baseline_in_poison)
+    beyond_theory_one(alpha_teacher, adaptive, student_state_dim, length, ext_length, eps, fixed_inputs, base_lr, epochs, num_baseline, num_special, with_baseline_in_poison, debug)
